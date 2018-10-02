3b4e6df1b463528ab6d88a07202662e5736e405b
Yagya Petersen <yagya@angleorange.co.za>
2018-10-02 12:14:50 +0200

    Updated

--- MainFile.js
+++ MainFile.js
@@ -1,23 +1,55 @@
+function initialize(screen) {
+    this.currentScreen = screen;
+}
+function setCurrentScreen(screen) {
+    this.currentScreen = screen;
+}
+function getCurrentScreen() {
+    return this.currentScreen;
+}
+
+var screens = {
+    first_input: 1,
+    second_input: 2,
+    questions: 3
+}
+
+
+
 var stdin = process.openStdin();
 var letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 var firstInput = [];
 var secondInput = [];
 
+initialize(screens.first_input);
+console.log("\nEnter the number of lines you'd like to encrypt:");
 stdin.addListener("data", function (a) {
-
-
-    console.log("Enter the number of lines you'd like to encrypt:\n");
-    firstInput.pop();
-    firstInput.push(parseInt(a));
-    console.log("First: " + firstInput);
-    for (i = 0; i <= firstInput; i++) {
-        console.log('test');
+    if (getCurrentScreen() == screens.first_input) {
+        var first = function (input) {
+            firstInput.pop();
+            firstInput.push(parseInt(input));
+            console.log("First: " + firstInput);
+        }
+        first(parseInt(a));
+        setCurrentScreen(screens.second_input);
+        console.log("\nEnter how much you'd like to shift the letters by:");
     }
 
-    console.log("Enter how much you'd like to shift the letters by:\n");
-    secondInput.push(a.toString());
-    console.log("Second: " + secondInput);
-    letters = letters.concat(letters.splice(0, parseInt(secondInput)));
-    console.log(letters.toString());
+    else if (getCurrentScreen() == screens.second_input) {
+        var second = function (input) {
+            secondInput.pop();
+            secondInput.push(input);
+            console.log("Second: " + secondInput);
+            letters = letters.concat(letters.splice(0, parseInt(secondInput)));
+            console.log(letters.toString());
+        }
+        second(parseInt(a));
+        setCurrentScreen(screens.questions);
+        console.log("\nEnter ["+ firstInput +"] sentences you would like to encrypt:")
+    }
 
+    else if (getCurrentScreen() == screens.questions) {
+
+    }
 });
+
