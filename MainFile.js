68fcc9b81f24af344a37ddc781f92f46a5102c79
Yagya Petersen <yagya@angleorange.co.za>
2018-10-04 12:27:13 +0200

    Finished App

--- MainFile.js
+++ MainFile.js
@@ -23,36 +23,39 @@ var sentences = [];
 
 initialize(screens.first_input);
 
-             
 console.log("______________________________________\n\x1b[31mWELCOME TO THE CAESER SHIFT CIPHER APP\x1b[0m\n\x1b[0m______________________________________");
 console.log("\n\x1b[31mEnter the number of lines you'd like to encrypt:\x1b[0m");
 stdin.addListener("data", function (a) {
     if (getCurrentScreen() == screens.first_input) {
-        if (isNaN(a)) {
+        if (isNaN(a) || a == false) {
+            console.log("\n\x1b[31mEnter the number of lines you'd like to encrypt:\x1b[0m");
             setCurrentScreen(screens.first_input);
         }
-        var first = function (input) {
-            firstInput.pop();
-            firstInput.push(parseInt(input));
-            console.log("\nFirst: " + firstInput);
+        else {
+            var first = function (input) {
+                firstInput.pop();
+                firstInput.push(parseInt(input));
+                console.log("\nFirst: " + firstInput);
+            }
+            first(parseInt(a));
+            setCurrentScreen(screens.second_input);
+            console.log("\n\x1b[31mEnter how much you'd like to shift the letters by:\x1b[0m");
         }
-        first(parseInt(a));
-        setCurrentScreen(screens.second_input);
-        console.log("\n\x1b[31mEnter how much you'd like to shift the letters by:\x1b[0m");
     }
-
     else if (getCurrentScreen() == screens.second_input) {
-        if (isNaN(a)) {
+        if (isNaN(a) || a == false) {
             setCurrentScreen(screens.second_input);
         }
-        var second = function (input) {
-            secondInput.pop();
-            secondInput.push(input);
-            console.log("\nSecond: " + secondInput);
+        else {
+            var second = function (input) {
+                secondInput.pop();
+                secondInput.push(input);
+                console.log("\nSecond: " + secondInput);
+            }
+            second(parseInt(a));
+            setCurrentScreen(screens.questions);
+            console.log("\x1b[31m\nEnter [\x1b[0m" + firstInput + "\x1b[31m] sentences you would like to encrypt: \x1b[0m")
         }
-        second(parseInt(a));
-        setCurrentScreen(screens.questions);
-        console.log("\x1b[31m\nEnter [\x1b[0m" + firstInput + "\x1b[31m] sentences you would like to encrypt: \x1b[0m")
     }
 
     else if (getCurrentScreen() == screens.questions) {
@@ -71,31 +74,31 @@ stdin.addListener("data", function (a) {
         if (a == false) {
             function encrypt(sentence, secondInput) {
 
-                var letters = "abcdefghijklmopqrstuvwxyz";
+                var letters = "abcdefghijklmnopqrstuvwxyz";
                 var newSentences = "";
-                
-                for (var i = 0; i < sentence.length; i++) {
-                    var character = sentence[i]
-                    isUpper = character === character.toUpperCase() ? true : false;
-                    character = character.toLowerCase();
 
-                    if (letters.indexOf(character) > -1) {
-                        var encrypted = letters.indexOf(character) + parseInt(secondInput);
+                for (var i = 0; i < sentence.length; i++) {
+                    var char = sentence[i];
+                    isUpper = char === char.toUpperCase() ? true : false;
+                    char = char.toLowerCase();
+
+                    if (letters.indexOf(char) > -1) {
+                        var encrypted = letters.indexOf(char) + secondInput;
                         if (encrypted < letters.length) {
                             isUpper ? newSentences += letters[encrypted].toUpperCase() : newSentences += letters[encrypted];
                         } else {
-                            var shifted = -(letters.length - newSentences);
+                            var shifted = -(letters.length - encrypted);
                             isUpper ? newSentences += letters[shifted].toUpperCase() : newSentences += letters[shifted];
                         }
                     }
 
                     else {
-                        newSentences += character;
+                        newSentences += char;
                     }
                 }
                 return newSentences;
             }
-            sentences = encrypt(sentences.toString(), secondInput);
+            sentences = encrypt(sentences.toString(), parseInt(secondInput));
             console.log("\n\x1b[31mHere is your encrypted message: \n_______________________\x1b[0m");
             console.log(sentences.toString());
             console.log("\x1b[31m_______________________\nCaesar will be proud of you !!\x1b[0m");
@@ -103,8 +106,8 @@ stdin.addListener("data", function (a) {
         else {
             console.log("You can only press enter");
             setCurrentScreen(screens.changer);
-        }process.exit(0);
+        } process.exit(0);
     }
-    
+
 });
 
