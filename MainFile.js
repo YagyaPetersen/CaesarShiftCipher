27cc633540829a4b004efd9df51dc2722ea25cec
Yagya Petersen <yagya@angleorange.co.za>
2018-10-05 11:40:50 +0200

    Fixing Bug

--- MainFile.js
+++ MainFile.js
@@ -1,9 +1,6 @@
 var readline = require('readline');
 var rl = readline.createInterface(process.stdin, process.stdout)
-var firstInput = [];
-var secondInput = [];
 var sentences = [];
-
 
 console.log("______________________________________\n\x1b[31mWELCOME TO THE CAESER SHIFT CIPHER APP\x1b[0m\n\x1b[0m______________________________________");
 
@@ -13,9 +10,7 @@ rl.question("\n\x1b[31mEnter the number of lines you'd like to encrypt:\x1b[0m\n
     }
     else {
         var first = function (input) {
-            firstInput.pop();
-            firstInput.push(parseInt(input));
-            console.log("\nFirst: " + firstInput);
+            console.log("\nFirst: " + input);
         }
         first(parseInt(answer1));
     }
@@ -25,59 +20,57 @@ rl.question("\n\x1b[31mEnter the number of lines you'd like to encrypt:\x1b[0m\n
         }
         else {
             var second = function (input) {
-                secondInput.pop();
-                secondInput.push(input);
-                console.log("\nSecond: " + secondInput);
+                console.log("\nSecond: " + input);
             }
             second(parseInt(answer2));
         }
 
-        rl.question("\x1b[31m\nEnter [\x1b[0m" + firstInput + "\x1b[31m] sentences you would like to encrypt: \x1b[0m\n", function (answer3) {
-            for (i = 0; i < firstInput; i++) {
-                var encryption = answer3;
+        rl.question("\x1b[31m\nEnter [\x1b[0m" + answer1 + "\x1b[31m] sentences you would like to encrypt: \x1b[0m\n", function (answer3) {
+            for (i = 0; i < parseInt(answer1); i++) {
+                var encryption = answer3
+                sentences.push(encryption);
+            };
+            console.log(sentences);
+            if (sentences.length = answer1) {
+                // console.log(sentences);
+                rl.question("\n\x1b[31mPress Enter to Encrypt message\x1b[0m\n", function (answer4) {
+                    if (answer4 == false) {
+                        function encrypt(sentence, secondInput) {
 
-            }
-            sentences.push(encryption);
+                            var letters = "abcdefghijklmnopqrstuvwxyz";
+                            var newSentences = "";
 
-            //if (i == sentences.length) {
-            rl.question("\n\x1b[31mPress Enter to Encrypt message\x1b[0m\n", function (answer4) {
-                if (answer4 == false) {
-                    function encrypt(sentence, secondInput) {
+                            for (var i = 0; i < sentence.length; i++) {
+                                var char = sentence[i];
+                                isUpper = char === char.toUpperCase() ? true : false;
+                                char = char.toLowerCase();
 
-                        var letters = "abcdefghijklmnopqrstuvwxyz";
-                        var newSentences = "";
-
-                        for (var i = 0; i < sentence.length; i++) {
-                            var char = sentence[i];
-                            isUpper = char === char.toUpperCase() ? true : false;
-                            char = char.toLowerCase();
-
-                            if (letters.indexOf(char) > -1) {
-                                var encrypted = letters.indexOf(char) + secondInput;
-                                if (encrypted < letters.length) {
-                                    isUpper ? newSentences += letters[encrypted].toUpperCase() : newSentences += letters[encrypted];
-                                } else {
-                                    var shifted = -(letters.length - encrypted);
-                                    isUpper ? newSentences += letters[shifted].toUpperCase() : newSentences += letters[shifted];
+                                if (letters.indexOf(char) > -1) {
+                                    var encrypted = letters.indexOf(char) + secondInput;
+                                    if (encrypted < letters.length) {
+                                        isUpper ? newSentences += letters[encrypted].toUpperCase() : newSentences += letters[encrypted];
+                                    } else {
+                                        var shifted = -(letters.length - encrypted);
+                                        isUpper ? newSentences += letters[shifted].toUpperCase() : newSentences += letters[shifted];
+                                    }
+                                }
+                                else {
+                                    newSentences += char;
                                 }
                             }
-                            else {
-                                newSentences += char;
-                            }
+                            return newSentences;
                         }
-                        return newSentences;
+                        sentences = encrypt(sentences.toString(), parseInt(answer2));
+                        console.log("\x1b[31mHere is your encrypted message: \n_______________________\x1b[0m");
+                        console.log(sentences.replace(",", ""));
+                        console.log("\x1b[31m_______________________\n\nCaesar will be proud of you !!\x1b[0m\n");
                     }
-                    sentences = encrypt(sentences.toString(), parseInt(secondInput));
-                    console.log("\n\x1b[31mHere is your encrypted message: \n_______________________\x1b[0m");
-                    console.log(sentences.replace(",", ""));
-                    console.log("\x1b[31m_______________________\nCaesar will be proud of you !!\x1b[0m\n");
-                }
-                else {
-                    console.log("You can only press enter");
-                    setCurrentScreen(screens.changer);
-                } process.exit(0);
+                    else {
+                        console.log("You can only press enter");
+                    } process.exit(0);
 
-            })
+                })
+            }
         })
     })
 })
